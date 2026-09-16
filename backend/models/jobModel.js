bb032415e6ff654a30ed75f35f94ad6b67c8fc05
Biplov Gyawali <biplovg@metropolia.fi>
2026-09-16 15:42:02 +0300

    [iter8] Add job virtual field and finalize frontend proxy

--- backend/models/jobModel.js
+++ backend/models/jobModel.js
@@ -2,23 +2,51 @@ const mongoose = require("mongoose");
 
 const jobSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  type: { type: String, required: true }, // e.g., Full-time, Part-time, Contract
-  description: { type: String, required: true },
+
+  type: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    required: true,
+  },
+
   company: {
     name: { type: String, required: true },
     contactEmail: { type: String, required: true },
     contactPhone: { type: String, required: true },
   },
-  location: { type: String, required: true }, // e.g., City, State, or Remote
-  salary: { type: Number, required: true }, // e.g., Annual or hourly salary
-  postedDate: { type: Date, default: Date.now }, // Date the job was posted
+
+  location: {
+    type: String,
+    required: true,
+  },
+
+  salary: {
+    type: Number,
+    required: true,
+  },
+
+  postedDate: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-//add  virtual field id
+// Virtual field for company name
+jobSchema.virtual("companyName").get(function () {
+  return this.company.name;
+});
+
+// Include virtual fields when converting documents to JSON
 jobSchema.set("toJSON", {
   virtuals: true,
   transform: (doc, ret) => {
     ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
     return ret;
   },
 });
@@ -26,4 +54,3 @@ jobSchema.set("toJSON", {
 const Job = mongoose.model("Job", jobSchema);
 
 module.exports = Job;
-
